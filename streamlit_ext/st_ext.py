8e42dd189cfaeb62caf3f3b0ce8d2281b49c0429
John Lyu <lvjunhong@citics.com>
2025-01-06 09:20:48 +0800

    more style

--- streamlit_ext/st_ext.py
+++ streamlit_ext/st_ext.py
@@ -102,36 +102,37 @@ def download_button(
     button_uuid = str(uuid.uuid4()).replace("-", "")
     button_id = re.sub(r"\d+", "", button_uuid)
     button_css = f"""
-        <style>
-            #{button_id} {{
-                background-color: rgb(255, 255, 255);
-                color: rgb(38, 39, 48);
-                padding: 0.25em 0.75em;
-                position: relative;
-                text-decoration: none;
-                border-radius: 0.5rem;
-                border-width: 1px;
-                border-style: solid;
-                border-color: rgba(49, 51, 63, 0.2);
-                border-image: initial;{custom_css}
-            }}
-            #{button_id}:hover {{
-                border-color: rgb(255, 75, 75);
-                color: rgb(255, 75, 75);
-            }}
-            #{button_id}:active {{
-                box-shadow: none;
-                background-color: rgb(255, 75, 75);
-                color: white;
-                }}
-        </style> """
+<style>
+    #{button_id} {{
+        background-color: rgb(255, 255, 255);
+        color: rgb(38, 39, 48);
+        padding: 0.25em 0.75em;
+        position: relative;
+        text-decoration: none;
+        border-radius: 0.5rem;
+        border-width: 1px;
+        border-style: solid;
+        border-color: rgba(49, 51, 63, 0.2);
+        border-image: initial;{custom_css}
+    }}
+    #{button_id}:hover {{
+        border-color: rgb(255, 75, 75);
+        color: rgb(255, 75, 75);
+    }}
+    #{button_id}:active {{
+        box-shadow: none;
+        background-color: rgb(255, 75, 75);
+        color: white;
+        }}
+</style>"""
 
     dl_link = (
         button_css
+        + "\n"
         + f'<a class="steDownloadButton" download="{file_name}" id="{button_id}" '
         f'href="data:file/txt;base64,{b64}">{label}</a><br></br>'
     )
 
-    div_dl_link = f"""<div class="row-widget stDownloadButton">{dl_link}</div>"""
+    div_dl_link = f"""<div class="row-widget stDownloadButton">\n{dl_link}\n</div>"""
     st.markdown(div_dl_link, unsafe_allow_html=True)
     return dl_link
